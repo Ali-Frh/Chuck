d0a44323f497c7e721487a4ea6b9583250ea9d18
Ali-Frh <alicowman79@gmail.com>
2024-03-29 02:19:36 +0430

    Proj's Skeleton

---
+++ Client/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import { Input, Stack } from '@chakra-ui/react'
+
+import Login from "./Login.jsx"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+function Home() {
+  return (
+      <h2>Suka </h2>
+    );
+  }
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          
+          <Route path="/" element={ <Home /> } />
+          
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/Login.jsx
@@ -0,0 +1,10 @@
+import {React} from "react";
+
+function Login() {
+  return (
+      <a> hah </a>
+    );
+
+}
+
+export default Login;
