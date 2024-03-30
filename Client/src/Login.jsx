88add29a18c6cc6322c2c0b9505881cdfdd67c68
Ali Frh <saintalifrh@disroot.org>
2024-03-31 03:52:16 +0430

    Basic form functionality + some cool things

--- Client/src/Login.jsx
+++ Client/src/Login.jsx
@@ -1,30 +1,92 @@
-import {React, useEffect} from "react";
+import {React, useEffect, useState, useRef} from "react";
 import chuck from "./assets/chuck.png"
 import  "./css/login.css"
 import { ChakraProvider } from "@chakra-ui/react"
 import { Input, Stack, Button } from '@chakra-ui/react'
+import io from "socket.io-client"
+import axios from "axios"
 
 function Login() {
+  const [UserLogin, setUserLogin] = useState()
+  const UserLoginElement = useRef();
+  const NextButt = useRef()
+  const EditButt = useRef()
+  const LoginElement = useRef()
+  const PassBox = useRef () 
+  const ShowHide = useRef()     
+  const Baloon = useRef() 
 
-  useEffect(() => {
-    const socket = new WebSocket("ws://127.0.0.1:5000/auth")
+  const ShowHideFunc = (el) => {
+    // console.log(el)
+    if ( el.target.innerHTML == "Show") {
+      el.target.innerHTML = "Hide";
+      PassBox.current.type="text";
+    } else {
+      el.target.innerHTML = "Show";
+      PassBox.current.type="password";
 
-    // Connection opened
-    socket.addEventListener("open", (event) => {
-      socket.send("Connection established")
-      socket.send("suka");
+    }
+  }
 
+  const EditMail = () => {
+    UserLoginElement.current.disabled=false;
+    LoginElement.current.style.display="none";
+    NextButt.current.style.display="block"
+    // ShowHide.current.st
+    EditButt.current.style.display = "none";
+    PassBox.current.value = "";
+  }
+
+  const LoginHandler = () => {
+    axios.post("http://127.0.0.1:5000/login",{
+        payload: UserLogin,
+        password: PassBox.current.value
+    }).then((response)=> {
+      console.log(response.data)
+      if (response.data == "login") {
+          
+      } else {
+
+      }
+    }).catch(()=>{
+      console. log("Fuxk")
     })
+  }
 
-    // Listen for messages
-    socket.addEventListener("message", (event) => {
-      console.log("Message from server ", event.data)
+  const nextHandle = () => {
+    
+    axios.post("http://127.0.0.1:5000/auth",{
+        payload: UserLogin
+    }).then((response)=> {
+      if (response.data == "found") {
+          UserLoginElement.current.disabled=true;
+          LoginElement.current.style.display="block";
+          NextButt.current.style.display="none"
+          // ShowHide.current.st
+          EditButt.current.style.display = "Block";
+      } else {
+
+      }
+    }).catch(()=>{
+      console. log("Fuxk")
     })
+    // this.setSocketListeners = this.setSocketListeners.bind(this)
 
-    // connection.current = ws
+    // const socket = new WebSocket("ws://127.0.0.1:5000");
+    // socket.addEventListener("open", () => {
 
-    return () => socket.close()
-  }, []);
+      // socket.send("suka")
+    // }
+    // );
+    // 
+    // state.socket.on('connect', () => {
+    //   console.log("Websocket connected: " + state.socket.connected)
+    // })
+    // state.socket.on('custom-server-msg', (data) => {
+    //   console.log("Data received: " + data.data)
+    // })
+
+  }
 
   return (
       // <a> hah </a>
@@ -34,12 +96,44 @@ function Login() {
           <div className="whole-page">
 
           <div className="card">
+            <div className="logo-wrapp"
+            onMouseEnter={() => {
+              Baloon.current.style.display="block";
+            }}
 
-            <img className="logo" src={chuck} alt="" />
+            onMouseLeave={
+              ()=> {
+                Baloon.current.style.display = "none"; 
+              }
+            }
+            >
+
+              <img className="logo" src={chuck} alt=""
+              
+              />
+              <div ref={Baloon} className="baloon">
+                DONT MOLEST ME!
+              </div>
+            </div>
             {/* <label htmlFor="mail"></label> */}
-            <p className="slogan">Hello I'm Chuck</p>
-            <input type="mail" placeholder="Email Address / Username" className="mail" />
-            <button className="next">Next</button>
+            {/* <p className="slogan">Hello I'm Chuck</p> */}
+              <div class="mail-holder">
+
+                <input type="mail" ref={UserLoginElement} onChange={(e)=>setUserLogin(e.target.value)} placeholder="Email Address / Username" className="mail" />
+                <i ref={EditButt } onClick={EditMail } class="edit">Edit</i>
+              </div>
+
+            <div className="stage-1">
+              <button className="next" ref={NextButt} onClick={nextHandle}>Next</button>
+            </div>
+
+            <div className="log-in" ref={LoginElement}>
+              <input ref={PassBox} type="password" onChange="" placeholder="Password" className="mail password" />
+              <i ref={ShowHide} onClick={(e) => ShowHideFunc(e)} class="show edit">Show</i>
+              <button className="next login" onClick={LoginHandler}>Login</button>
+              <i className="forgoot">i'm so silly that i forgot my password</i>
+            </div>
+
           </div>
 
           </div>
--- Server/main.py
+++ Server/main.py
@@ -1,24 +1,145 @@
-from flask import Flask, render_template
+from flask import Flask, request
+# Needed for localhost testing.
+from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO, emit
+import sqlite3, time
+
+db = []
+
+def db_init(): 
+
+    # Create a database and open the database.
+    # If the database already exists just opens the database
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    # Create a users table if the table does not exists
+    c.execute('''CREATE TABLE IF NOT EXISTS users(
+              uid INTEGER,
+              name TEXT,
+              username TEXT,
+              mail TEXT,
+              password TEXT, 
+              restriction TEXT,
+              signup_time INTEGER,
+              last_seen INTEGER,
+              avatar TEXT
+              
+              )''')
+    current_unix_time = int(time.time())
+
+    # c.execute(f'''INSERT INTO users VALUES(
+    #           1,
+    #           "Ali",
+    #            "Shakak",
+    #            "ali@shak.net" ,
+    #           "81dc9bdb52d04dc20036dbd8313ed055" , 
+    #           "none",
+    #           {current_unix_time},
+    #           {current_unix_time},
+    #           "null"
+              
+    #           )''')
+    # commit changes and close database connect
+    conn.commit()
+    conn.close()
+
+def db_connection(): 
+
+    # Create a database and open the database.
+    # If the database already exists just opens the database
+    conn = sqlite3.connect('data.db',check_same_thread=False )
+    return conn 
+    # conn.close()
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Socket io setup.
+app.config['SECRET_KEY'] = 'secret!'
+# |cors_allowed_origins| is required for localhost testing.
+socket = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on('message')
-def handle_message(msg):
-    print('Message: ' + msg)
-    emit('message', msg, broadcast=True)
+# For localhost testing.
+CORS(app)
 
-@socketio.on('auth')
-def login_handler(msg):
-    print('Message: ' + msg)
-    emit('message', msg, broadcast=True)
+
+@socket.on('connect')
+def test_connect():
+    emit('custom-server-msg',
+         {'data': 'Print this out via data.data in your client'})
+
+@app.route("/auth",methods=['GET', 'POST'] ) 
+def authentication():
+    print("auuuuuth")
+    # form = RegisterForm(request.form)
+    with sqlite3.connect("data.db") as conn:
+        c = conn.cursor()
+        payload = request.get_json()["payload"]
+        # print()
+        c.execute(f"SELECT * FROM users WHERE username='{payload}' or mail='{payload}';")
+        res= c.fetchall()
+        if len(res) == 0:
+            return "not found"
+        else:
+            return "found"
+    return "failure", 500
+    # print("yeaa")
+    # return "200" 
+import hashlib
+  
+# initializing string
+str2hash = "GeeksforGeeks"
+  
+# encoding GeeksforGeeks using encode()
+# then sending to md5()
+# result = hashlib.md5(str2hash.encode())
+
+@app.route("/login",methods=['GET', 'POST'] ) 
+def authentication_login():
+    # print("auuuuuth")
+    # form = RegisterForm(request.form)
+    with sqlite3.connect("data.db") as conn:
+        c = conn.cursor()
+        payload = request.get_json()["payload"]
+        password = request.get_json()["password"] 
+        hash =  hashlib.md5(password.encode()).hexdigest()
+        # print()
+        c.execute(f"SELECT * FROM users WHERE password='{hash}' AND username='{payload}' or mail='{payload}';")
+        res= c.fetchall()
+        if len(res) == 0:
+            return "wrong"
+        else:
+            return "true buddy"
+    return "failure", 500
 
 if __name__ == '__main__':
-    socketio.run(app)
+    db_init() #move it to later func 
+    db = db_connection()
+    print("Starting websocket server")
+    socket.run(app, allow_unsafe_werkzeug=True)
+        
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO, emit
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'secret'
+# socketio = SocketIO(app)
+
+# # @app.route('/')
+# # def index():
+# #     return render_template('index.html')
+
+# @socketio.on('message')
+# def handle_message(msg):
+#     print('Message: ' + msg)
+#     emit('message', msg, broadcast=True)
+
+# @socketio.on('connect')
+# def login_handler():
+#     msg = "anap"
+#     print('Message: ' + msg)
+#     emit('message', msg, broadcast=True)
+
+# if __name__ == '__main__':
+#     socketio.run(app, allow_unsafe_werkzeug=True)
         
